ee94c4f70aae7ed5d82e5891346444c96c9ee94d
Erika <erikaek@student.chalmers.se>
2018-03-01 16:26:16 +0100

    debugging

--- src/caffe/layers/softmax_loss_layer.cu
+++ src/caffe/layers/softmax_loss_layer.cu
@@ -67,6 +67,8 @@ void SoftmaxWithLossLayer<Dtype>::Forward_gpu(
   // Similarly, this memory is never used elsewhere, and thus we can use it
   // to avoid having to allocate additional GPU memory.
   Dtype* counts = prob_.mutable_gpu_diff();
+  LOG(INFO) << this->type()
+      << "bottom size:" + bottom.size();
   if( bottom.size() == 2) {
     // original version with equally weighted pixels
     // NOLINT_NEXT_LINE(whitespace/operators)
@@ -91,7 +93,7 @@ void SoftmaxWithLossLayer<Dtype>::Forward_gpu(
     caffe_gpu_asum(nthreads, counts, &valid_count);
     if( valid_count == 0 ) {
       LOG(INFO) << this->type()
-		<< " warning: sum of pixel wise loss weights is zero!";
+    << " warning: sum of pixel wise loss weights is zero!";
     }
   }
   if ( valid_count == 0) {
