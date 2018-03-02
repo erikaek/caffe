d238d7f00ca3ca563d51ee08662b717b622470d9
Erika <erikaek@student.chalmers.se>
2018-03-02 14:09:32 +0100

    debugging

--- src/caffe/layers/softmax_loss_layer.cu
+++ src/caffe/layers/softmax_loss_layer.cu
@@ -91,7 +91,7 @@ void SoftmaxWithLossLayer<Dtype>::Forward_gpu(
     caffe_gpu_asum(nthreads, counts, &valid_count);
     if( valid_count == 0 ) {
       LOG(INFO) << this->type()
-    << bottom.size();
+    << " warning: sum of pixel wise loss weights is zero!";
     }
   }
   if ( valid_count == 0) {
@@ -200,6 +200,8 @@ void SoftmaxWithLossLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
       caffe_gpu_asum(nthreads, counts, &valid_count);
     }
     Dtype loss_weight = 0;
+    LOG(INFO) << this->type()
+              << "Backward_gpu";
     if( valid_count == 0) {
         LOG(INFO) << this->type()
                   << " warning: sum of pixel wise loss weights is zero!";
