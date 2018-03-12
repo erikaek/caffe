1093b9648f3f42aef92d599b8e969223a2e7daae
Erika <erikaek@student.chalmers.se>
2018-03-12 13:24:30 +0100

    debugging

--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -223,7 +223,9 @@ void Solver<Dtype>::Step(int iters) {
     }
     loss /= param_.iter_size();
     // average the loss across iterations for smoothed reporting
+    
     UpdateSmoothedLoss(loss, start_iter, average_loss);
+
     if (display) {
       LOG_IF(INFO, Caffe::root_solver()) << "Iteration " << iter_
           << ", loss = " << smoothed_loss_;
